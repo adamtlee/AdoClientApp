feb52689360d54c09892615ec7efb0cd3e838479
adamtlee <adam.lee346@gmail.com>
2020-05-03 19:45:02 -0600

    Read SQLDataAdapter

--- ADOClientApp/Program.cs
+++ ADOClientApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace ADOClientApp
@@ -9,18 +10,27 @@ namespace ADOClientApp
         {
             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ADOClientDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             SqlConnection conn = new SqlConnection(connectionString);
-            string querystring = "Select * from AdoClient";
             conn.Open();
-            SqlCommand cmd = new SqlCommand(querystring, conn);
-           
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + reader[3].ToString() + reader[4].ToString());
-            }
+            ReadData(conn); 
+            // InsertData(conn); 
 
             // Close the connection String
             conn.Close();
+        }
+
+        public static void InsertData(SqlConnection conn)
+        {
+            // string insertmethod = "INSERT INTO ADOClient (Id, Name, Description, Abbreviation, ClientType) VALUES()"
+        }
+
+        public static void ReadData(SqlConnection conn)
+        {
+            string querystring = "Select * from AdoClient";
+            SqlDataAdapter adapter = new SqlDataAdapter(querystring, conn);
+            DataSet clientData = new DataSet();
+            adapter.Fill(clientData, "AdoClient");
+            Console.WriteLine(clientData.GetXml());
+            Console.ReadKey();
         }
     }
 }
