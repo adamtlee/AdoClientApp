c7115e88b5e476e24c78db266861af5c90586aa2
adamtlee <adam.lee346@gmail.com>
2020-05-03 22:11:22 -0600

    Added Menu

--- ADOClientApp/Program.cs
+++ ADOClientApp/Program.cs
@@ -16,11 +16,42 @@ namespace ADOClientApp
             DataSet clientData = new DataSet();
             adapter.Fill(clientData, "AdoClient");
 
-            CreateClient(adapter, clientData);
+            menu:
+            Console.WriteLine("Welcome to the client App. ");
+            Console.Write(
+                "Enter the following commands to perform Crud operations" + "\n" +
+                "Enter c to create a new client" + "\n" +
+                "Enter r to read the client" + "\n" + 
+                "Enter u to update a client" + "\n" +
+                "Enter d to delete a client" + "\n");
+
+            string userInput = Console.ReadLine().ToLower(); 
+            switch (userInput)
+            {
+                case "c":
+                    CreateClient(adapter, clientData);
+                    goto menu;
+                case "r":
+                    ReadClient(adapter, clientData);
+                    goto menu;
+                case "u":
+                   UpdateClient(adapter, clientData);
+                    goto menu;
+                case "d":
+                    DeleteClient(adapter, clientData);
+                    goto menu;
+                default:
+                    Console.WriteLine("Incorrect Input");
+                    break;
+            }
+
+            // CreateClient(adapter, clientData);
 
             // ReadClient(adapter, clientData); 
-             
 
+            // UpdateClient(adapter, clientData); 
+
+            DeleteClient(adapter, clientData);
             // Close the connection String
             conn.Close();
         }
@@ -57,6 +88,16 @@ namespace ADOClientApp
         {              
             Console.WriteLine(clientData.GetXml());
             Console.ReadKey();
+        }
+
+        public static void UpdateClient(SqlDataAdapter adapter, DataSet clientData)
+        {
+            Console.WriteLine("Update Method");
+        }
+
+        private static void DeleteClient(SqlDataAdapter adapter, DataSet clientData)
+        {
+            Console.WriteLine("Delete Method");
         }
     }
 }
