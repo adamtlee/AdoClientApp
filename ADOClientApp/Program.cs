988ca768c4af2913f224b5a71007cb3432642ea4
adamtlee <adam.lee346@gmail.com>
2020-05-03 18:58:27 -0600

    Working read

--- ADOClientApp/Program.cs
+++ ADOClientApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace ADOClientApp
 {
@@ -6,7 +7,20 @@ namespace ADOClientApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ADOClientDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            string querystring = "Select * from AdoClient";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(querystring, conn);
+           
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + reader[3].ToString() + reader[4].ToString());
+            }
+
+            // Close the connection String
+            conn.Close();
         }
     }
 }
